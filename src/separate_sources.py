e1666fa2f4d0470828625d3fcee6000a0087ded7
mokoli <mnokoli@gmail.com>
2017-04-21 12:10:03 -0400

    try to get cross-correlation working

--- src/separate_sources.py
+++ src/separate_sources.py
@@ -1,11 +1,25 @@
 from scipy.io import wavfile
+import scipy.signal as scp
+import numpy as np
 
-path = "./out.wav"
+path = "../soundfiles/out6.wav"
+
+print "here1"
 
 rate, data = wavfile.read(path)
 # left is the data from channel 0.
-left = data[:, 0]
+print "here2"
+left = data[44000:45000, 0]
 # right is the data from channel 1.
-right = data[:, 1]
+print "here3"
+print left[1:10]
+right = data[44000:45000, 1]
+print right[1:10]
 
-print left
+print "here4"
+print len(right)
+corr = scp.correlate(right, left)
+print "here5"
+res = max(corr)
+
+print res
