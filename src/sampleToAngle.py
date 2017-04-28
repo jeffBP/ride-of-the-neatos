99c4a92ecd7b3dc7721f66cc06cb231d8d993cef
Jeff Pflueger <jeffrey.pflueger@students.olin.edu>
2017-04-28 11:45:09 -0400

    Added sampleToAngly.py, to convert sample difference to angle of source

---
+++ src/sampleToAngle.py
@@ -0,0 +1,25 @@
+import math
+
+def sampleToAngle(n):
+    """
+    Function that takes the sample difference between 2 mics
+    and returns the angle away that the sound source is.
+    Arguments: Number of Samples (n)
+    Returns: Angle of source (theta)
+    """
+    if n > 42.695:
+        x = 42.695
+    elif n < -42.695:
+        x = -42.695
+    else:
+        x = n
+    A = 340.29/(44000*0.0254)
+    denom = math.sqrt(20905*A**2*x**2 - A**4 * x**4)
+    numer = 1872
+    theta = math.acos(denom/numer)
+    if n  < 0:
+        theta = -theta
+    return theta
+
+if __name__ == "__main__":
+    print(math.degrees(sampleToAngle(20)))
