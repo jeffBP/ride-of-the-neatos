503df9831b042f8c8495c6aed18ecb38d6268f8f
SeanCCarter <seancarter26@gmail.com>
2017-05-04 20:17:10 -0400

    Ok, look. It MOSTLY works right now.

--- src/pursuit.py
+++ src/pursuit.py
@@ -4,10 +4,16 @@
 # Author: Sean Carter
 # Python Version: 2.7
 
-import getAngle #Our functions to deal with sound correlation
+from getAngle import getSamples, sampleToAngle #Our functions to deal with sound correlation
 import math
-from tf.transformations import quaternion_from_euler
+import os
+import subprocess
+import rospy
+from threading import Thread
+from geometry_msgs.msg import Twist
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from visualization_msgs.msg import Marker
+from nav_msgs.msg import Odometry
 
 def make_marker(angle):
 	""" Creates Marker for a sphere at a specified location
@@ -19,10 +25,10 @@ def make_marker(angle):
 			A Marker, pointed in the direction the robot
 			thinks it has heard sound from."""
 	my_marker = Marker(type=Marker.ARROW)
-	my_marker.header.frame_id = "base_link"
+	my_marker.header.frame_id = "odom"
 
 	#We only rotate in one plaen, quaternion only uses two values
-	quaternion = quaternion_from_euler(math.radians(angle))
+	quaternion = quaternion_from_euler(math.radians(angle),0.0,0.0)
 	my_marker.pose.orientation.x = quaternion[0]
 	my_marker.pose.orientation.w = quaternion[3]
 
@@ -32,52 +38,98 @@ def make_marker(angle):
 	my_marker.scale.z = .1
 	return my_marker
 
+def constant_record_and_copy(callback):
+	while True:
+		print "Recording new file."
+		#These lines record the audio, and copy it onto our computer
+		subprocess.call('ssh pi@192.168.17.201 /home/pi/use_case_scripts/ride-of-the-neatos/Record_from_lineIn_Micbias_modified.sh > /dev/null', shell=True)
+		subprocess.call('scp pi@192.168.17.201:/home/pi/wavFile.wav ~/catkin_ws/src/ride-of-the-neatos/src', shell=True)
+		callback()
+		# err = os.system("ssh pi@192.168.17.201 /home/pi/use_case_scripts/ride_of_the_neatos/Record_from_lineIn_Micbias.sh > /dev/null")
+		# if err != 0:
+		# 	print "Error recording on Pi"
+		#
+		# err = os.system("scp pi@192.168.17.201:/home/pi/wavFile.wav ~/catkin_ws/src/ride-of-the-neatos/src")
+		# if err != 0:
+		# 	print "Error copying file from Pi"
+
+def get_distance(angle1, angle2):
+    phi = abs(angle2-angle1) % 2*math.pi
+    sign = 1
+    # used to calculate sign
+    if not ((angle1-angle2 >= 0 and angle1-angle2 <= math.pi) or (
+            angle1-angle2 <= -math.pi and angle1-angle2 >= -2*math.pi)):
+        sign = -1
+    if phi > math.pi:
+        result = math.pi-phi
+    else:
+        result = phi
+
+    return result*sign
+
+
 class audioFollower(object):
 	""" Creates a ros node to command a neato to follow an audio source"""
 	def __init__(self):
 		rospy.init_node("audioFollower")
-		self.r = rospy.Rate(5)
+		rospy.Subscriber("/odom", Odometry, self.process_position)
 		self.commander = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-		self.arrow = rospy.Publisher("/direction", Marker, queue_size=10)
-		self.angle = None
+		self.arrow = rospy.Publisher("/audio_src", Marker, queue_size=10)
+		self.angle_history = []
+		self.target_angle = None
+		self.theta = None
+		self.r = rospy.Rate(10)
 
-		#Waits for first measurement to start following the audio
-		while self.angle == None:
-			self.r.sleep()
 
-    def createVelocity(self):
-		""" Creates proportional controller to follow audio source:
-			the further is is from an area in front of the
-			robot, the faster it tries to center it
+	def createVelocity(self):
+		""" Creates proportional controller to center on last
+			recieved location of audio source: the further it is
+			from an area in front of the robot, the faster
+			it tries to center it
 
 			Returns:
 				Twist message: telling the neato how fast to turn,
 				and how fast to move forwards (not fast).
 		"""
 		spin = Twist()
-		spin.angular.z = .9*(self.angle/90.0)
-		spin.linear.x = .2
+		distance = .4*get_distance(self.target_angle, self.theta)
+		spin.angular.z = -.4*get_distance(self.target_angle, self.theta)
+		#spin.linear.x = .2
 		return spin
 
-	def findAudioAngle(self):
-		#These lines record the audio, and copy it onto our computer
-		os.system("ssh pi@192.168.17.201 /home/pi/use_case_scripts/ride_of_the_neatos/Record_from_lineIn_Micbias.sh")
-        os.system("scp pi@192.168.17.201:~/use_case_scripts/ride_of_the_neatos/wavFile.wav ~/catkin_ws/src/ride_of_the_neatos/src")
-
+	def updateTargetAngle(self):
 		#Use functions created to identify audio angle
 		samps = getSamples("wavFile.wav")
-	    ang = sampleToAngle(samps)
-		self.angle = ang
+		print "Getting audio"
+		ang = sampleToAngle(samps)
+		print ang
+		self.target_angle = self.theta + math.radians(ang)
+		self.publishAngle()
+
+	def process_position(self, m):
+		""" Get theta from pose (geometry_msgs.Pose)"""
+		pose = m.pose.pose
+		orientation_tuple = (pose.orientation.x,
+							 pose.orientation.y,
+							 pose.orientation.z,
+							 pose.orientation.w)
+		angles = euler_from_quaternion(orientation_tuple)
+		self.theta = angles[2]
 
 	def publishAngle(self):
-		arrow_marker = make_marker(self.angle)
+		arrow_marker = make_marker(self.target_angle)
 		self.arrow.publish(arrow_marker)
 
-    def run(self):
+	def run(self):
+		thread = Thread(target = constant_record_and_copy, args = (self.updateTargetAngle, ))
+		thread.start()
+		while self.target_angle == None:
+			self.r.sleep()
 		while not rospy.is_shutdown():
-			self.findAudioAngle()
-			self.publishAngle()
-			self.commander.publish(self.createVelocity())
+			#self.commander.publish(self.createVelocity())
+			#print "Angling."
+			#print self.target_angle
+			#print self.theta
 			self.r.sleep()
 
 if __name__ == '__main__':
